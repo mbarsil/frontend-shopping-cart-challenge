90373aa8513af254da37466f924537b9fee525db
mbarsil <barrajon87@gmail.com>
2019-06-29 17:52:22 +0200

    feat(cabify-button) : Implemented generic cabify button

--- src/app/shared/components/cabify-button/cabify-button.component.ts
+++ src/app/shared/components/cabify-button/cabify-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'cabify-button',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cabify-button.component.scss']
 })
 export class CabifyButtonComponent implements OnInit {
+
+  @Input() text: string;
 
   constructor() { }
 
