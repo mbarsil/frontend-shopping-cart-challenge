6b66981cab967fcab2216d814159d10558bdd401
mbarsil <barrajon87@gmail.com>
2019-06-29 13:18:36 +0200

    feat(cabify-header) : Created basic version of generic header component

--- src/app/shared/components/cabify-header/cabify-header.component.ts
+++ src/app/shared/components/cabify-header/cabify-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'cabify-header',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cabify-header.component.scss']
 })
 export class CabifyHeaderComponent implements OnInit {
+
+
+  @Input() text: string;
 
   constructor() { }
 
